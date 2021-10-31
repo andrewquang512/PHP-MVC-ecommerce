2ab2361940f075ac744afb7223c061a94c43a0c8
andrewquang <andrew.viquang@gmail.com>
2021-10-31 21:41:57 +0700

    delete all

--- application/views/admin/templates/footer.php
+++
@@ -1,4 +0,0 @@
-<script type="text/javascript" src="../../../../public/assets/js/admin/index.js"></script>
-<script type="text/javascript" src="../../../../public/assets/bootstrap/js/jquery.min.js"></script>
-<script type="text/javascript" src="../../../../public/assets/bootstrap/js/popper.min.js"></script>
-<script type="text/javascript" src="../../../../public/assets/bootstrap/js/bootstrap.min.js"></script>
