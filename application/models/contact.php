ae405b75aaf550d9642b72f734c5e4070d7dd20f
andrewquang <andrew.viquang@gmail.com>
2021-11-11 11:49:31 +0700

    update to branch master

--- application/models/contact.php
+++ application/models/contact.php
@@ -1,28 +1,6 @@
 <?php
-class Contact{
-    public $id;
-    public $customer_name;
-    public $title;
-    public $phone;
-    public $email;
-    public $content;
 
-    function __construct($id, $customer_name, $title, $phone, $email, $content){
-        $this->id = $id;
-        $this->customer_name = $customer_name;
-        $this->title = $title;
-        $this->phone = $phone;
-        $this->email = $email;
-        $this->content = $content;
-    }
-
-    static function all(){
-        $list = [];
-        $db = DB::getInstance();
-        $req = $db->query('SELECT * FROM CONTACT');
-        foreach ($req->fetchAll() as $item){
-            $list[] = new Contact($item['id'], $item['customer_name'],$item['title'],$item['phone'],$item['email'],$item['content']); 
-        }
-        return $list;
-    }
+class Contact extends DB {
+    
 }
+?>
