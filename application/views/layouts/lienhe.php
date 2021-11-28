65233e1602fb10b38b8ca0d618281381fc79e339
Trieu Le <trieule0406@gmail.com>
2021-11-28 11:57:22 +0700

    fix responsive cho trang lien he

--- application/views/layouts/lienhe.php
+++ application/views/layouts/lienhe.php
@@ -9,7 +9,7 @@
     include('head.php');
   ?>
     <link rel="stylesheet" href="http://localhost/PHP-MVC-ecommerce/public/assets/css/homepages/header.css?v=<?php echo time()?>" type="text/css">
-    <link rel="stylesheet" href="http://localhost/PHP-MVC-ecommerce/public/assets/css/homepages/lienhe.css" type="text/css">
+    <link rel="stylesheet" href="http://localhost/PHP-MVC-ecommerce/public/assets/css/homepages/lienhe.css?v=<?php echo time()?>" type="text/css">
 
 </head>
 
@@ -30,9 +30,9 @@
           <div>
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d2888.6677700318323!2d106.80477820172293!3d10.878269096993685!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1svi!2s!4v1606618103249!5m2!1svi!2s"
-              width="400" height="377" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false"
+              width=100% height="377" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false"
               tabindex="0"></iframe>
-            <label> <i align="center" style="padding-top:20px;">Địa chỉ tọa lạc</i></label>  
+            <label> <i align="center" style="margin-top:50px;">Địa chỉ tọa lạc</i></label>  
           </div>
         </div>
 
